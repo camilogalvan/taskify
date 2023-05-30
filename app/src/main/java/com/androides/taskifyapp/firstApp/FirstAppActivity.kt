cb446342aa569f17a471c9884243d5acb780f637
Carlos Quinones <carlosalejandroqm@ufps.edu.co>
2023-05-30 01:08:14 -0500

    initial commit

---
+++ app/src/main/java/com/androides/taskifyapp/firstApp/FirstAppActivity.kt
@@ -0,0 +1,12 @@
+package com.androides.taskifyapp.firstApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.androides.taskifyapp.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_app)
+    }
+}
