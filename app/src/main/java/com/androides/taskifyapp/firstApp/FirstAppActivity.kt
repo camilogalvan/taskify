17c049075cefa1d20f546ce4e773e12825ae58f9
BrayanGuerreroXD <bg7047653@gmail.com>
2023-06-25 22:10:18 -0500

    add changes to firstAppActivity

--- app/src/main/java/com/androides/taskifyapp/firstApp/FirstAppActivity.kt
+++ app/src/main/java/com/androides/taskifyapp/firstApp/FirstAppActivity.kt
@@ -6,18 +6,15 @@ import android.os.Bundle
 import com.androides.taskifyapp.R
 
 class FirstAppActivity : AppCompatActivity() {
-
     // Duración en milisegundos de la pantalla de presentación (en este caso, 3 segundos)
     private val SPLASH_TIME_OUT = 900L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_app)
-
         android.os.Handler().postDelayed({
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
             finish()
         }, SPLASH_TIME_OUT)
-
     }
 }
